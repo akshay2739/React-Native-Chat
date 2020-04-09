57408362d0df97bddafa7b1105d6d4a884eb909a
akshay2739 <akshay2739@gmail.com>
2020-04-09 21:22:23 +0530

    Navigation added

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import {NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import Main from './components/Main'
+import Chat from './components/Chat'
+import Home from './components/Home'
+
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} options={{ title: 'Overview' }} />
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { Text, View , StyleSheet } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity 
+                    style={styles.Button}
+                    onPress={()=> this.props.navigation.navigate('Main') }   
+                >    
+                    <Text style={styles.ButtonText}>Main</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                    style={styles.Button}
+                    onPress={ ()=> this.props.navigation.navigate('Chat') } 
+                >
+                    <Text style={styles.ButtonText}>Chat</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    Button:{
+        backgroundColor:'red',
+        margin:10
+    },
+    ButtonText:{
+        fontSize:25,
+        padding:5
+    }
+  });
